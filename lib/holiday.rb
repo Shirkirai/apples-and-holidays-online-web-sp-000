da175289ed690c06c36d4215da57fde9441d3a74
Jung Lee <lee.jung.s95@gmail.com>
2020-05-02 17:27:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,6 +77,7 @@ def all_supplies_in_holidays(holiday_hash)
         array.each do |x|
           last_array << x.capitalize!
           last_array.join(" ")
+        end
       puts last_array.join() : supply.join()
     end
   end
