60bbb1586a54846d5cd8351d6aa61c8fa97ebb1e
Jung Lee <lee.jung.s95@gmail.com>
2020-05-02 18:11:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -96,6 +96,7 @@ def all_holidays_with_bbq(holiday_hash)
       end
     end
   end
+  bbq_array
 end
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
