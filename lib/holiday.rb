12e431c407f181d32ff220af0526f132e94f373d
Jung Lee <lee.jung.s95@gmail.com>
2020-05-02 18:01:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -91,7 +91,6 @@ def all_holidays_with_bbq(holiday_hash)
       array = []
       if supplies.include?("BBQ") == true
         array << specific_holiday
-        puts array
       end
     end
   end
