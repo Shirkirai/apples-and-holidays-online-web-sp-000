8fcd4f033bcc9017371e1462b46be94f0ab5c194
Jung Lee <lee.jung.s95@gmail.com>
2020-05-02 17:56:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -86,9 +86,6 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-end
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
   holiday_hash.each do |season, values|
     values.each do |specific_holiday, supplies|
       array = []
@@ -98,7 +95,7 @@ end
     end
 
   end
-
-    end
-  end
 end
+  # return an array of holiday names (as symbols) where supply lists
+  # include the string "BBQ"
+  
